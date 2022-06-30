d180508588d6d1c2e66538865ceba69ad6e145bf
Aaron Chris <rooneychris83@gmail.com>
2022-06-30 12:14:45 +0100

    Started css

--- note.js
+++ note.js
@@ -40,14 +40,35 @@ showNoteBtn.addEventListener("click", () => {
   noteInput.value = "";
 });
 
-function showNotes(){
-    let NoteAdd = ''
-    document.querySelectorAll('.note').forEach(note => note.remove());
-    allNotes.forEach((note, index) => {
-        let filtDesc = note.description.replaceAll("\n", '<br />')
-        NoteAdd += `<div class="note">
+function showNotes() {
+  let NoteAdd = "";
+  document.querySelectorAll(".note").forEach((note) => note.remove());
+  allNotes.forEach((note, index) => {
+    let filtDesc = note.description.replaceAll("\n", "<br />");
+    NoteAdd += `<div class="note">
         <h3 class="note-title">${note.title}</h3>
-        
-        </div>`
-    })
+        <p class="note-body">${note.description}</p>
+        <hr>
+        <p class="date">${note.date}</p>
+        <i onclick="showMenu(this)" class="fa-soild fa-ellipsis"></i>
+        <div class="settings">
+        <div class="menu">
+        <span class="edit-btn"
+        onclick="editNote(${index}, '${note.title}', '${filtDesc}')"><i class="fa-soild fa-pen></i>Edit
+        </span>
+        <span class="delete-btn"
+        onclick="deleteNote(${index})"><i class="fa-soild fa-trash></i>Delete
+        </span>
+        </div>
+        </div>
+        </div>`;
+  });
+  noteBox.innerHTML =
+    NoteAdd ||
+    ` <span><i class="fa-soild fa-note-sticky></i>
+    </span>
+    <span class="no-notes-message">No Notes here yet</i>
+    </span>`;
 }
+
+showNotes();
